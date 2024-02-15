fb006e67f4452e51c831f08d878be9a155502602
Suraj Biradar <suraj.biradar@hotmail.com>
2024-02-16 00:44:08 +0530

    grid count paths

--- src/main/java/org/drogo/CountPaths.java
+++ src/main/java/org/drogo/CountPaths.java
@@ -1,5 +1,6 @@
 package org.drogo;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -26,11 +27,54 @@ public class CountPaths {
         }
 
         List<Integer> pos = List.of(row, col);
+        if (memo.containsKey(pos)) {
+            return memo.get(pos);
+        }
 
-        return countPaths(row + 1, col, grid, memo) + countPaths(row, col + 1, grid, memo);
+        int result = countPaths(row + 1, col, grid, memo) + countPaths(row, col + 1, grid, memo);
+        memo.put(pos, result);
+        return result;
 
 
     }
+
+    public static List<String> printPaths(
+            int row,
+            int col,
+            List<List<String>> grid,
+            HashMap<List<Integer>, List<String>> memo
+    ) {
+        List<String> result = new ArrayList<>();
+        if (row == grid.size() || col == grid.get(0).size()) {
+            return new ArrayList<>();
+        }
+
+        if ("X".equals(grid.get(row).get(col))) {
+            return new ArrayList<>();
+        }
+        if (row == grid.size() - 1 && col == grid.size() - 1) {
+            result.add("[" + row + "," + col + "]");
+        }
+
+        List<Integer> pos = List.of(row, col);
+        if (memo.containsKey(pos)) {
+            return memo.get(pos);
+        }
+
+        List<String> downPaths = (printPaths(row + 1, col, grid, memo));
+        List<String> rightPaths = (printPaths(row, col + 1, grid, memo));
+        for (String rightPath : rightPaths) {
+            result.add("[" + row + "," + col + "]" + rightPath);
+        }
+        for (String downPath : downPaths) {
+            result.add("[" + row + "," + col + "]" + downPath);
+        }
+        memo.put(pos, result);
+        return result;
+
+
+    }
+
 
     public static void main(String[] args) {
         List<List<String>> grid = List.of(
@@ -40,6 +84,7 @@ public class CountPaths {
         );
 
         System.out.println(countPaths(grid));
+        System.out.println(printPaths(0, 0, grid, new HashMap<>()));
     }
 
 
